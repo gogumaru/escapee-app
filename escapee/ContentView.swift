82248ae83234e748248fa11e236ad5d241043a81
anin <banindya10@gmail.com>
2026-06-04 09:25:42 +0700

    Initial Commit

---
+++ escapee/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  escapee
+//
+//  Created by Benedikta Anin on 04/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ escapee/escapeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  escapeeApp.swift
+//  escapee
+//
+//  Created by Benedikta Anin on 04/06/26.
+//
+
+import SwiftUI
+
+@main
+struct escapeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
